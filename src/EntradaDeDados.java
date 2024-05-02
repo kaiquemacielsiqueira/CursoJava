7bac9d2a1ac5307d018a806e7811b0373ac786fe
Kaique Siqueira <kaiquemaciels1@hotmail.com>
2024-05-01 22:41:10 -0300

    Primeiros codigos deste curso

---
+++ src/EntradaDeDados.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class EntradaDeDados {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		double x;
+		System.out.println("Digite o valor: ");
+		x = sc.nextDouble();
+		System.out.println("Você digitou " + x);
+		System.out.printf("Você digitou: %.1f%n", x);
+		// Observe que o printf saiu com virgula e o println com ponto, pois no brasil o
+		// padrão é virgula mas o println faz a conversão sozinho, para imprimir com
+		// virgua preciso colocar o printf.
+
+		char y;
+		System.out.println("Digite a palavra para mostrar a primeira letra: ");
+		y = sc.next().charAt(0);
+		System.out.println("Você digitou " + y);
+		// na hora que eu chamei o leitor, chamei junto uma função para armazenar o
+		// char, como é para armazenar apenas uma letra, você pode até digitar mais, mas
+		// só ira mostrar a primeira letra da palavra.
+		sc.close();
+
+	}
+
+}
---
+++ src/TesteProcessamento.java
@@ -0,0 +1,29 @@
+
+public class TesteProcessamento {
+
+	public static void main(String[] args) {
+
+		int y, x;
+		x = 5;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(y);
+
+		int b, B, h;
+		b = 6;
+		B = 8;
+		h = 5;
+		int area = (b + B) / 2 * h;
+		System.out.println(area);
+
+		int e, f;
+		double resultado;
+		e = 5;
+		f = 2;
+		resultado = (double) 5 / 2; // Daria 2.0, pq divisão de numeros interioros, mas colocando double ele faz um
+									// casting, que seria a conversão explicita destes valores.
+		System.out.println(resultado);
+
+	}
+
+}
