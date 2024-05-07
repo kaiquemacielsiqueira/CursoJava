18a9f9d0fcaeecbfb20917be9ac1bb25853c3051
Kaique Siqueira <kaiquemaciels1@hotmail.com>
2024-05-07 11:24:17 -0300

    Ultima atualização

---
+++ ModulosDoCurso/EstruturaCondicional/ExemploIfElse.java
@@ -0,0 +1,17 @@
+package EstruturaCondicional;
+
+public class ExemploIfElse {
+
+	public static void main(String[] args) {
+
+		int x = 5;
+
+		System.out.println("Bom dia");
+
+		System.out.println("Boa tarde");
+
+		System.out.println("Boa noite");
+
+	}
+
+}
