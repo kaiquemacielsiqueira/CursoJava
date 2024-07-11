ba5d9f4354494aee45037c44502efa9c0aebf7df
Kaique Siqueira <kaiquemaciels1@hotmail.com>
2024-07-11 10:19:29 -0300

    Ultima atualização

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -23,7 +23,7 @@ public class Program2 {
 		System.out.println("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println(product); // Esta fazendo chamada pra product, que entende que tem um toString lá
 
 		sc.close();
 
