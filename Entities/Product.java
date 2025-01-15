48a226d025a6423009b73d68edaf39671b765d8e
Kaique Siqueira <kaiquemaciels1@hotmail.com>
2025-01-15 08:09:22 -0300

    Estudando construtores

--- Entities/Product.java
+++ Entities/Product.java
@@ -5,6 +5,12 @@ public class Product {
 	public double price;
 	public int quantity;
 
+	// Construtor geralmente depois dos atributos da classe (poderia ser outros
+	// nomes mas por conveção usamos o mesmo nome).
+	public Product(String name, double price, int quantity) {
+
+	}
+
 	public double totalValueStock() {
 		return price * quantity;
 	}
